83615ecb48165e6d27aebe0ce9cc3ed9c40670c6
nguyennguyen <nguyen.nguyen@terralogic.com>
2017-06-06 20:13:19 +0700

    Support predict target_price, stop_loss_price

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # stock_trading
 using python to trade stock
+
+Requirements:
+# Installation
+# sudo apt-get install python-bs4
--- python_beautiful_soup.py
+++ python_beautiful_soup.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
-#Installation
-#sudo apt-get install python-bs4
 from bs4 import BeautifulSoup
 import requests
 import re
 import time
+
+# Will be improvement
+# target_price, stop_loss_price
+expect_dict = {'MSN': [44.1, 0] ,'LHG': [19.9, 0], 'QNS':[100.0, 0], 'HD2': [13.9, 0], 'HID': [4.13, 0], 'HVG': [7.25, 0], 'TYA': [12.4, 0]}
 
 def displayStock( stock_url ):
 	r = requests.get(stock_url)
@@ -27,22 +29,43 @@ def displayStock( stock_url ):
 	#str1 = ''.join(str(e) for e in tag_down)
 	#print str1
 	str2 = ''.join(tag_down[1])
+#	print str2
 #	print str2.lstrip()
 
-	print soup.title.string.lstrip()[0:3] + "\t" + tag_point[0] + "\t"+ str2
-	#print re.findall(r'\d+', str2) #Not correct now
+	pattern = re.compile(r'\s+')
+	sentence = re.sub(pattern, '\t', str2.lstrip())
+#	print sentence
+	print soup.title.string.lstrip()[0:3] + "\t" + tag_point[0] + "\t"+ sentence + "\t",
+
+#	Predict & compare target value here
+	MaCK = soup.title.string.lstrip()[0:3]
+	GiaHT = tag_point[0]
+	for key, values in expect_dict.iteritems():
+		if MaCK == key:
+#			print MaCK + "\t" +str(expect_dict[key])
+			target_price = expect_dict[key][0]
+			stop_loss_price = expect_dict[key][1]
+			predict_message = ""
+			if ( float(target_price) > float(GiaHT) ) : predict_message = " => Vuot qua ky vong, theo doi"
+			elif ( float(target_price) == float(GiaHT) ) : predict_message = " => Bang gia ky vong, ban"
+			elif ( float(target_price) < float(GiaHT) ) : predict_message = " => Thap hon gia ky vong, theo doi"
+			elif ( float(stop_loss_price) >= float(GiaHT) ) : predict_message = " => Cut-loss"
+			print "target_price:" + " "+ str(target_price) + " "+ "stop_loss_price:" + " "+ str(stop_loss_price) + predict_message
+			#print "target_price" + " "+ str(target_price) + "Gia Hien Tai" + " "+ str(GiaHT) + "=>"+ predict_message
 
 # main
-count = 0
-while (count < 1):
-	print 'The count is:', count
-	count = count + 1
-   	with open('stock_url.txt') as f:
-		for lines in f.read().splitlines():
+#count = 0
+#while (count < 1):
+	#print 'The count is:', count
+	#count = count + 1
+print "The time of maximum pessimism is the best time to buy"
+print "and the time of maximum optimism is the best time to sell."
+with open('stock_url.txt') as f:
+	for stock_url in f.read().splitlines():
 	#		print lines
-			displayStock(lines)
-	f.close() 
+		displayStock(stock_url)
+f.close() 
 	# Wait for 5 seconds
-	time.sleep(3)
+#	time.sleep(3)
 
 print "Good bye!"
